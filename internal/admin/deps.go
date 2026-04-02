dfc827abea5829889b2d8785ce492d82e546477a
Youzini-afk <13153778771cx@gmail.com>
2026-04-03 01:24:02 +0800

    fix: add compat accessors to admin store interface

--- internal/admin/deps.go
+++ internal/admin/deps.go
@@ -24,6 +24,7 @@ type ConfigStore interface {
 	IsEnvWritebackEnabled() bool
 	HasEnvConfigSource() bool
 	ConfigPath() string
+	CompatEffective() config.EffectiveCompat
 	SetVercelSync(hash string, ts int64) error
 	AdminPasswordHash() string
 	AdminJWTExpireHours() int
